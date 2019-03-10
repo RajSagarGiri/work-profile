bf0f987fd229e498e516d844503a1ea52fe0e507
Raj Sagar <therajsagar@gmail.com>
2019-03-10 23:31:00 +0530

    initial commit

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../styles/body.css';
-
+import Experience from './workcard.jsx';
 
 class Body extends React.Component{
 
     render(){
+
+        const arr = this.props.work.work_experiences.map( i=> <Experience val ={i}/>)
+
         return(
         <div className='work-skill'>
             <div className='work-edu'>
-                <div className='work'>W1</div>
+                <div className='work'>{arr}</div>
                 <div className='edu'>Sch1</div>
             </div>
-            <div className='w-skill'>fucking</div>
+            <div className='w-skill'>mySkill</div>
         </div>
         )
     }
---
+++ src/components/workcard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Experience = (props) => (
+    <div className='work-card'>
+        <div className='logo-s'>
+            <img src={props.val.company.avatar_url} className='company-logo'/>
+        </div>
+        <div className='right-det'>
+             <div className='detail-s'>
+                 <h3 className='company-name'>{props.val.company.name}</h3>
+                 <h5 className='designation'>{props.val.designation}</h5>
+                 {props.val.location && <h5 className='location'>{props.val.location}</h5>}
+                 <h5 className='duration'>{props.val.duration}</h5>
+             </div>
+             <div className='work-details'>
+                            {props.val.description && <h5>{props.val.description.split('&')[0]}</h5>}
+                            {props.val.skills_data.length>0 && <div style={{display: 'flex'}}>{props.val.skills_data.map(i => <div className='sk'>{i.skill_name}</div>)}</div>}
+             </div>
+        </div>
+    </div>
+)
+
+export default Experience;
