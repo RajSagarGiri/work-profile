dc009c6665254582088cd011aae97e6cd219ba55
Raj Sagar <therajsagar@gmail.com>
2019-03-13 01:58:29 +0530

    Updates

---
+++ precache-manifest.aaaa675addde900954c9797eb636900d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29d86b08938fda526f2b",
+    "url": "/work-profile/static/js/runtime~main.855ce4d3.js"
+  },
+  {
+    "revision": "83926d3fc6e4ec1877d4",
+    "url": "/work-profile/static/js/main.a6a87459.chunk.js"
+  },
+  {
+    "revision": "560be65e6208a1765f79",
+    "url": "/work-profile/static/js/2.1106cecd.chunk.js"
+  },
+  {
+    "revision": "83926d3fc6e4ec1877d4",
+    "url": "/work-profile/static/css/main.58b2754e.chunk.css"
+  },
+  {
+    "revision": "67feadda432f20cde894da5eb24ba541",
+    "url": "/work-profile/index.html"
+  }
+];
